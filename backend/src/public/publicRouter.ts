cee8faf1bd9759af3efa671dda1a64a9c3b65c27
Alex Hendrix <hendrixtothemax38@gmail.com>
2025-06-08 13:38:28 -0600

    added basic routing for .js and .css files

--- backend/src/public/publicRouter.ts
+++ backend/src/public/publicRouter.ts
@@ -6,12 +6,35 @@ const router = Router();
 
 // Example: GET /public/info
 router.get('/info', (req: Request, res: Response) => {
-  res.send('This is from the public router!');
+    res.send('This is from the public router!');
 });
 
-// Example: serve a file
-router.get('/html', (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, '../../public/index.html'));
+router.get('/css/:cssFileName', (req: Request, res: Response) => {
+  const cssFileName = req.params.cssFileName;
+
+  // Sanitize and resolve the full path
+  const filePath = path.resolve(__dirname, `../../../frontend/css/${cssFileName}`);
+
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      console.error(err);
+      res.status(404).send('CSS file not found');
+    }
+  });
+});
+
+router.get('/js/:jsFileName', (req: Request, res: Response) => {
+  const jsFileName = req.params.jsFileName;
+
+  // Sanitize and resolve the full path
+  const filePath = path.resolve(__dirname, `../../../frontend/js/${jsFileName}`);
+
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      console.error(err);
+      res.status(404).send('CSS file not found');
+    }
+  });
 });
 
 export default router;
