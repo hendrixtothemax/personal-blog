b354c4503882262ac7ca6f75b35f79cc396c050d
Alex Hendrix <hendrixtothemax38@gmail.com>
2025-06-26 21:03:02 -0600

    added htmx and icon handling

--- backend/main.go
+++ backend/main.go
@@ -9,8 +9,16 @@ import (
 
 func main() {
 	http.Handle("/foo", http.HandlerFunc(fooHandler))
+
 	wrapped := http.HandlerFunc(indexHandler)
 	http.Handle("/", LoggingMiddleware(wrapped))
+
+	wrapped = http.HandlerFunc(faviconHandler)
+	http.Handle("/favicon.ico", LoggingMiddleware(wrapped))
+
+	wrapped = http.HandlerFunc(htmxHandler)
+	http.Handle("/js/htmx.js", LoggingMiddleware(wrapped))
+
 	fmt.Println("Server Starting! localhost:8080/")
 	http.ListenAndServe("0.0.0.0:8080", nil)
 }
@@ -26,6 +34,44 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/html; charset=utf-8")
 
 	file, err := os.Open("./index.html")
+	if err != nil {
+		http.Error(w, "Something went wrong!", http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+    if err != nil {
+        http.Error(w, "Something went wrong!", http.StatusInternalServerError)
+		return
+    }
+
+	w.Write(data)
+}
+
+func htmxHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/javascript; charset=utf-8")
+
+	file, err := os.Open("./htmx.min.js")
+	if err != nil {
+		http.Error(w, "Something went wrong!", http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+    if err != nil {
+        http.Error(w, "Something went wrong!", http.StatusInternalServerError)
+		return
+    }
+
+	w.Write(data)
+}
+
+func faviconHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "image/png; charset=utf-8")
+
+	file, err := os.Open("./favicon.png")
 	if err != nil {
 		http.Error(w, "Something went wrong!", http.StatusInternalServerError)
 		return
